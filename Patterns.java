3a30a474e409b865d43f14116e672ea4d8352adf
ABMK444 <akhan.12.4572@gmail.com>
2026-09-12 15:48:58 +0530

    initial commit

---
+++ Patterns.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+
+
+public class Patterns{
+    public static void main(String[] args){
+        boolean run = true;
+
+        try(Scanner read = new Scanner(System.in)){
+            do {
+            System.out.println("What would you like to print? \n \n 0. END PROGRAM \n 1. Square \n 2. Hollow Square \n 3. Triangle \n 4. Inverse Triangle \n 5. Solid Pyramid \n");
+            System.out.println("======================================================");
+            int input = read.nextInt();
+            if (input == 0){
+                run = false;
+            }
+            else if (input>5 || input<0) {
+                System.out.println("======================================================\n Only input from the options given fool.\n ======================================================");         
+
+            }
+            else {
+                System.out.println("======================================================\n Put in a number \n");
+                int n = read.nextInt();
+                switch (input) {
+                    case 1:
+                        Square(n);
+                        break;
+                    case 2:
+                        HollowSquare(n);
+                        break;
+                    case 3:
+                        Triangle(n);
+                        break;
+                    case 4:
+                        InvTriangle(n);
+                        break;
+                    
+                    case 5:
+                        Pyramid(n);
+                }
+            }} while (run); 
+
+        }
+       
+    }
+
+    public static void Square(int n){
+        for (int row= 0; row<n; row++){
+            System.err.println("* ".repeat(n));
+        }
+        System.out.println("\n======================================================");
+
+    }
+
+    public static void HollowSquare(int n){
+        for (int row = 1; row<=n; row++){
+            if (row == 1 || row==n){
+                System.out.println("* ".repeat(n));
+            }
+            else{
+                System.out.println("* "+"  ".repeat(n-2)+"* ");
+            }
+        }
+        System.out.println("\n======================================================");
+
+    }
+
+    public static void Triangle(int n){
+        for (int i=1; i<=n; i++){
+            System.err.println("");
+            for (int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+        }
+        System.out.println("\n======================================================");
+
+    }
+
+    public static void InvTriangle(int n){
+        for (int i=0; i<n; i++){
+            System.out.println();
+            for (int j = 0; j<(n-i); j++ ){
+                System.out.print("* ");
+            }
+        }
+        System.out.println("\n==========================================");
+    }
+    
+    public static void Pyramid(int n){
+        for (int i=1; i<=n; i++){
+            System.out.println();
+            for (int j = 1; j<=n; j++){
+                if (j<=n-i){System.out.print(" ");}
+                else {System.out.print("* ");}
+            }
+
+        }
+        System.out.println("\n==========================================");
+    }
+    
+
+}
+
